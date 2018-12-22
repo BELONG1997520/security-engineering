528c4607c1d9a2dd8126123e6b4e746f7172591e
ZC <zcoranger1997@163.com>
2018-12-23 00:27:59 +0800

    框架搭建

---
+++ security-engineering-parent/security-engineering-devicemanager/src/main/java/securityengineering.devicemanager/provider/IndexController.java
@@ -0,0 +1,26 @@
+package securityengineering.devicemanager.provider;
+
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Record;
+import io.jboot.Jboot;
+import io.jboot.web.controller.JbootController;
+import io.jboot.web.controller.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RequestMapping("/devicemanager")
+public class IndexController extends JbootController {
+    public void index() {
+        renderText("Hello World Jboot");
+    }
+
+    public void dbtest(){
+        List<Record> records = Db.find("select * from user");
+        renderText(Arrays.toString(records.toArray()));
+    }
+
+    public static void main(String[] args) {
+        Jboot.run(args);
+    }
+}
