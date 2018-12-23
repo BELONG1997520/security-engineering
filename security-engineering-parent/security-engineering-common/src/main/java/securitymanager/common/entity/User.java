949f31b53d21a4ac809b71e639c01d373171aba1
wanghanduo <810535096@qq.com>
2018-12-23 23:24:19 +0800

    test2

---
+++ security-engineering-parent/security-engineering-common/src/main/java/securitymanager/common/entity/User.java
@@ -0,0 +1,31 @@
+package securitymanager.common.entity;
+
+public class User {
+    private static int id;
+    private  static String login_name;
+    private  static String password;
+
+    public static int getId() {
+        return id;
+    }
+
+    public static void setId(int id) {
+        User.id = id;
+    }
+
+    public static String getLogin_name() {
+        return login_name;
+    }
+
+    public static void setLogin_name(String login_name) {
+        User.login_name = login_name;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+
+    public static void setPassword(String password) {
+        User.password = password;
+    }
+}
